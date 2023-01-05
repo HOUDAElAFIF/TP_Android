b10f274c17254ee698ae1b967ec1a750bc3bbbea
houda <elafife.houda@student.emi.ac.ma>
2023-01-05 12:57:21 +0100

    First commit

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+    EditText editText1;
+    EditText editText2;
+    String number;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editText1 = findViewById(R.id.username);
+        editText2 = findViewById(R.id.password);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("username",editText1.getText());
+        intent.putExtra("password",editText2.getText());
+        startActivity(intent);
+    }
+}
